76dbbf5aa7aab77c95e367a21f246b5885ebf637
mahdi <mahdi77em2@gmail.com>
2020-10-17 10:27:11 +0330

    added comments to errorAndLogs.go

--- server/errorAndLogs.go
+++ server/errorAndLogs.go
@@ -7,6 +7,11 @@ import (
 
 //We may want logging processes here . . .
 
+// logError just logs errors yet for the development progress
+// it gets a scope which is the scope of code that the err is came from
+// it gets an error which is the error that just happened
+// it gets a bool to know that the error is a serious one or not
+// if isSerious is True then it will fatal the server after logging err
 func logError(scope string, err error, isSerious bool) {
 
 	go playBeep()
