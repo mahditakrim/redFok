f53d73d4580f9b4aae976cb82f322cedf2a11112
mahdi <mahdi77em2@gmail.com>
2020-10-17 10:20:27 +0330

    added comments to deleter.go

--- server/deleter.go
+++ server/deleter.go
@@ -5,6 +5,8 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+// deleter is a controller pointer method that handles user deletion
+// it gets a websocket connection pinter and uses it as the user connection that will be deleted
 func (c *controller) deleter(conn *websocket.Conn) {
 
 	userName := c.checkAuthentication(conn)
